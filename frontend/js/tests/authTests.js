bfe9e68e1cb6758c9a01ca968072c8223432ac5e
Matias Melano <matiasfiorentino006@gmail.com>
2026-06-03 11:09:14 -0300

    Tetst de password corta

--- frontend/js/tests/authTests.js
+++ frontend/js/tests/authTests.js
@@ -45,3 +45,30 @@ testUtils.createTestButton("Test Login - Usuario Incorrecto (Juan y 12345)", asy
         testUtils.setSuccess(btn);
     }
 });
+
+testUtils.createTestButton("Test Registro - Contraseña Corta", async (btn) => {
+    const payload = {
+        username: 'pepe',
+        password: '1'
+    };
+
+    const response = await fetch('/api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+    });
+
+    const data = await response.json();
+    
+    testUtils.log(data, !response.ok);
+
+    if (response.status === 400) {
+        testUtils.setSuccess(btn);
+    } else {
+        btn.className = "w3-button w3-block w3-section w3-round w3-red";
+        testUtils.log({
+            error: `Se esperaba HTTP 400 (Bad Request), pero el servidor respondió HTTP ${response.status}`,
+            respuestaServidor: data
+        }, true);
+    }
+});
